b3764b6a72b0831d91260be3fdfc7507d8cf256d
Ab-Romia <aabouroumia@gmail.com>
2025-07-18 12:48:19 +0300

    Transaction Service

---
+++ transaction-service/src/main/java/com/virtualbank/transaction_service/exception/GlobalExceptionHandler.java
@@ -0,0 +1,33 @@
+package com.virtualbank.transaction_service.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<Object> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, Object> response = new HashMap<>();
+        Map<String, String> errors = new HashMap<>();
+
+        ex.getBindingResult().getAllErrors().forEach(error -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+
+        response.put("status", HttpStatus.BAD_REQUEST.value());
+        response.put("error", "Bad Request");
+        response.put("message", "Invalid request parameters");
+
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
+    }
+}
